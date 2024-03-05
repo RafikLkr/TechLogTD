e22b44d1de0dfeefcb18d04b46529d1c2291133f
Rafik Lekouara <abdelrafik.lekouara@universite-paris-saclay.fr>
2024-03-05 15:26:57 +0100

    Modification de la classe Team

--- team.py
+++ team.py
@@ -1,15 +1,15 @@
 import abc
 
 class Team(abc.ABC):
-  def __init__(self, members):
+  def __init__(self):
     self._members = []
     
   @abc.abstractmethod  
   def __len__(self):
-    pass
+    return len(self._members)
   
   @abc.abstractmethod
-  def __getitem__(self):
-    pass
+  def __getitem__(self, i):
+    return self._members[i]
   
   
