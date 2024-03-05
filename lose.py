72388fbab90dfad82ddb4c699615b07875e6bfc4
SULLIVAN LUONG <sullivanluong@gmail.com>
2024-03-05 16:03:24 +0100

    Modification de la classe Lose : - changement des valeurs à 0

--- lose.py
+++ lose.py
@@ -1,8 +1,8 @@
 class Lose:
     def __init__(self, nb_warriors, nb_hunters, nb_wizards):
-        self.nb_warriors = nb_warriors
-        self.nb_hunters = nb_hunters
-        self.nb_wizards = nb_wizards
+        self.nb_warriors = 0
+        self.nb_hunters = 0
+        self.nb_wizards = 0
 
     def __repr__(self):
         return f"Loses(nb_warriors={self.nb_warriors}, nb_hunters={self.nb_hunters}, nb_wizards={self.nb_wizards})"
