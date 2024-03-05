63cdd574bc85d59775f2e4164fd19ff7e3f13a9d
Rafik Lekouara <abdelrafik.lekouara@universite-paris-saclay.fr>
2024-03-05 14:33:41 +0100

    Code Classe warrior

--- warrior.py
+++ warrior.py
@@ -1,1 +1,9 @@
-fezokf
+import random 
+
+class Warrior:
+  def __init__(self):
+    self.degat = random.choice(range(3, 5))
+    self.chance = 5
+    self.fuite = 3
+    self.prix = 10
+    self.type = "warrior"
