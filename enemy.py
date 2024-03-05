3a22e9b2ce191a7855802514bef4c24158d5d55d
Rafik Lekouara <abdelrafik.lekouara@universite-paris-saclay.fr>
2024-03-05 16:12:41 +0100

    Modification des classes Enemy PlayerTeam et Game

--- enemy.py
+++ enemy.py
@@ -1,20 +1,26 @@
 from team import Team
 
 class Enemy(Team):
-  def __init__(self, unit, damage, loot):
+  def __init__(self, unit):
     super().__init__()
     self.__unit = unit
-    self.__damage = damage
-    self.__loot = loot
+    self.__damage = 0
+    self.__loot = 0
     
   def get_name(self):
     return self.__unit
     
   def get_damage(self):
-    return self.__damage
+    damage = 0
+    for member in self._members :
+      damage = damage + member.degat
+    return damage
   
   def get_loot(self):
-    return self.__loot
+    loot = 0
+    for member in self._members :
+      loot = loot + member.loot
+    return loot
   
   def get_members_type(self):
     types_of_members = [type(member) for member in self._members]
--- game.py
+++ game.py
@@ -3,3 +3,7 @@ class Game :
         self.nb_warriors = 0
         self.nb_hunters = 0
         self.nb_wizards = 0
+        self.player_name = ""
+        
+    def config(self):
+        self.player_name = input("Entrez votre nom de joueur : ")
